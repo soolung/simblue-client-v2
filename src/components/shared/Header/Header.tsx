0fb2c4385484042a287ae62039c2af94120d9db5
kimhyerin0909 <khyerin0909@gmail.com>
2023-02-01 22:30:01 +0900

    project setting

---
+++ src/components/shared/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./Header.style";
+
+export const Header = () => {
+  return (
+    <div>
+      <S.Logo src="/assets/logo.svg" />
+    </div>
+  );
+};
