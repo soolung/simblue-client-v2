57208c48915e567c1dc828766826b2086ff44866
kimhyerin0909 <khyerin0909@gmail.com>
2023-02-04 00:03:50 +0900

    ADD :: 프로필 팝업

---
+++ src/components/shared/Header/profilePopover/ProfilePopover.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import styled from "styled-components";
+import { userState } from "../../../../atoms/user";
+import { Colors } from "../../../../constants/colors";
+
+export const ProfilePopover = ({ close }: { close: Function }) => {
+  const navigate = useNavigate();
+  const setUser = useSetRecoilState(userState);
+  return (
+    <Popover>
+      <button onClick={() => navigate("/user/update/password")}>비밀번호 변경</button>
+      <button
+        onClick={() => {
+          localStorage.clear();
+          setUser({
+            accessToken: null,
+            refreshToken: null,
+            authority: null,
+            name: null,
+          });
+          navigate("/");
+          close();
+        }}>
+        로그아웃
+      </button>
+      <Ggoranji />
+    </Popover>
+  );
+};
+
+const Popover = styled.div`
+  display: flex;
+  position: absolute;
+  width: 300%;
+  top: 65px;
+  right: 0;
+  padding: 10px;
+  border-radius: 15px;
+  flex-direction: column;
+  background-color: white;
+  box-shadow: 0 16px 20px hsl(0deg 0% 68% / 20%);
+
+  button {
+    border-radius: 15px;
+    border: none;
+    background-color: white;
+    padding: 10px 0;
+    &:hover {
+      background-color: ${Colors.lightGray};
+    }
+  }
+`;
+
+const Ggoranji = styled.div`
+  position: absolute;
+  right: 10%;
+  bottom: 100%;
+  height: 11px;
+  overflow: hidden;
+  margin-bottom: -1px;
+
+  &::after {
+    content: "";
+    margin-top: 4px;
+    border: 1px solid $light-grey;
+    background-color: #fff;
+    height: 14px;
+    width: 14px;
+    display: block;
+    border-top-right-radius: 30%;
+    -webkit-transform: rotate(-55deg) skewX(-20deg);
+    transform: rotate(-55deg) skewX(-20deg);
+  }
+`;
