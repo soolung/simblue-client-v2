2fdf1ea534fc24368b6055541d57531793a087bd
kimhyerin0909 <khyerin0909@gmail.com>
2023-02-04 00:00:47 +0900

    ADD :: route 설정
    기본 라우팅을 설정하고, 둘러보기 페이지와 만들기 페이지는 로그인이 필요합니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
 import React from "react";
-import { Header } from "./components/shared/Header/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { Layout } from "./components/layout/Layout";
+import { AuthRequired } from "./components/shared/authRequired/AuthRequired";
+import { Create } from "./components/shared/create/Create";
+import { Header } from "./components/shared/header/Header";
+import { Login } from "./pages/auth/login/Login";
+import { Look } from "./pages/auth/look/Look";
+import { SignUp } from "./pages/auth/signUp/SignUp";
+import { Home } from "./pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Header />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route
+              path="/look"
+              element={
+                <AuthRequired>
+                  <Look />
+                </AuthRequired>
+              }
+            />
+            <Route
+              path="/create"
+              element={
+                <AuthRequired>
+                  <Create />
+                </AuthRequired>
+              }
+            />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
