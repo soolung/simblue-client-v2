fed2d7cb767322dfc799324e8d43d0c30063712b
kimhyerin0909 <khyerin0909@gmail.com>
2023-03-02 15:15:02 +0900

    ADD :: banner component

---
+++ src/constants/banner.ts
@@ -0,0 +1,10 @@
+export const bannerImg = [
+  {
+    img: "https://ifh.cc/g/SkMFmp.png",
+    linkTo: "",
+  },
+  {
+    img: "https://media.discordapp.net/attachments/1014696258293739533/1051118704504606772/Layer-4.jpg",
+    linkTo: "",
+  },
+];
