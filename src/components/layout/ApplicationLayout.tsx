4efb8a015c9dad1d5048485055ef5cba5157729a
kimhyerin0909 <khyerin0909@gmail.com>
2023-03-06 08:27:12 +0900

    CHORE :: application 반응형 (#5)

--- src/components/layout/ApplicationLayout.tsx
+++ src/components/layout/ApplicationLayout.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 type Props = { children: React.ReactNode; bgColor?: string };
 
 export const ApplicationLayout = ({ children, bgColor }: Props) => {
-  return <div style={{ display: "flex", width: "100%", gap: "2%", flexDirection: "row", marginTop: "3%" }}>{children}</div>;
+  return <LayoutStyle>{children}</LayoutStyle>;
 };
+
+const LayoutStyle = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 2%;
+  row-gap: 1.3vh;
+  flex-direction: row;
+  margin-top: 3%;
+  flex-wrap: wrap;
+`;
--- src/components/shared/application/Application.style.ts
+++ src/components/shared/application/Application.style.ts
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-export const AppBox = styled.div`
+export const AppBox = styled(Link)`
   display: flex;
   flex-direction: column;
   width: 23.5%;
@@ -8,15 +9,29 @@ export const AppBox = styled.div`
   justify-content: space-between;
 
   aspect-ratio: 1/1;
+  color: black;
+  text-decoration: none;
   border: 1px solid #d9d9d9;
   border-radius: 18px;
+
+  @media screen and (max-width: 950px) {
+    width: 31.3%;
+  }
+  @media screen and (max-width: 700px) {
+    width: 47%;
+    padding: 5%;
+  }
 `;
 export const TopBox = styled.div`
   display: flex;
   justify-content: space-between;
   width: 100%;
-  font-size: 1.8vw;
+  font-size: 25px;
   margin-bottom: 3%;
+
+  @media screen and (max-width: 700px) {
+    font-size: 18px;
+  }
 `;
 
 export const Title = styled.span`
