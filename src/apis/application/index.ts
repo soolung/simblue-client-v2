71fd84fac351c0c0b93bcc6a3b77de017a2a2097
kimhyerin0909 <khyerin0909@gmail.com>
2023-03-06 19:43:56 +0900

    CHORE :: 함수 이름 규칙 변경

--- src/apis/application/index.ts
+++ src/apis/application/index.ts
@@ -1,9 +1,9 @@
 import server from "../client";
 
-export const GET_APPLICATION = async (type: string) => {
+export const getApplication = async (type: string) => {
   return (await server.get(`/application?type=${type}`)).data;
 };
 
-export const GET_FOUR_LATEST_APPLICATION = async () => {
+export const getFourLatestApplication = async () => {
   return (await server.get("/application/paging")).data;
 };
