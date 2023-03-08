dcf64639ffcd1cf90c7413ae497632db4284641f
kimhyerin0909 <khyerin0909@gmail.com>
2023-03-08 09:51:23 +0900

    CHORE :: question 기본 컴포넌트

---
+++ src/pages/application/detail/question/Question.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { QUESTION } from "../../../../apis/@types/application";
+
+export const Question = ({ quest }: { quest?: QUESTION[] }) => {
+  return <div>Question</div>;
+};
