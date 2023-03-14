ed4c2f5234b309d7086fd80f8669cb72b3ca4243
kimhyerin0909 <khyerin0909@gmail.com>
2023-03-14 16:34:30 +0900

    CHORE :: button type

--- src/components/shared/button/Button.tsx
+++ src/components/shared/button/Button.tsx
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { ButtonHTMLAttributes } from "react";
 import { ButtonStyle } from "./Button.style";
 
-type Props = { text: string; event: React.MouseEventHandler<HTMLButtonElement>; disabled: boolean };
+interface BtnProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  event: React.MouseEventHandler<HTMLButtonElement>;
+  disabled: boolean;
+}
 
-export const Button = ({ text, event, disabled = false }: Props) => {
-  return (
-    <ButtonStyle onClick={!disabled ? event : () => {}} className={disabled ? "disabled" : ""}>
-      {text}
-    </ButtonStyle>
-  );
+export const Button = (props: BtnProps) => {
+  return <ButtonStyle {...props} className={props.disabled ? "disabled" : ""} />;
 };
