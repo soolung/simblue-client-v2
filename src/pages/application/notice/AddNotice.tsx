ad3b3bf8b5275fd51ed4b5dc3a81b4e0b2a31d9e
kimhyerin0909 <khyerin0909@gmail.com>
2023-03-20 19:22:25 +0900

    ADD :: notice 관련 api

---
+++ src/pages/application/notice/AddNotice.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useMutation, useQueryClient } from "react-query";
+import styled from "styled-components";
+import { addNotice } from "../../../apis/notice";
+import { Colors } from "../../../constants/colors.constant";
+import { GET_APPLICATION_RESULT } from "../../../constants/keys/application.key";
+
+export const AddNotice = ({ applicationId }: { applicationId: number }) => {
+  const [notice, setNotice] = useState<string>("");
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(() => addNotice(applicationId, notice), {
+    onSuccess: () => {
+      setNotice("");
+      queryClient.invalidateQueries([GET_APPLICATION_RESULT]);
+    },
+  });
+
+  return (
+    <WriteNotice>
+      <NoticeInput value={notice} onChange={(e) => setNotice(e.target.value)} />
+      <AddNoticeBtn onClick={() => (notice !== "" ? mutate() : alert("공지를 작성해주세요."))}>공지 하기</AddNoticeBtn>
+    </WriteNotice>
+  );
+};
+
+const WriteNotice = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 100px;
+  @media screen and (max-width: 960px) and (min-width: 500px) {
+    flex-direction: column;
+    height: 120px;
+    row-gap: 5px;
+  }
+`;
+
+const NoticeInput = styled.textarea`
+  width: 70%;
+  height: 100%;
+  padding: 5%;
+  background-color: white;
+  border-radius: 15px;
+  border: 1px solid ${Colors.gray};
+  font-size: 18px;
+  resize: none;
+  outline: none;
+
+  @media screen and (max-width: 960px) and (min-width: 500px) {
+    width: 100%;
+  }
+`;
+
+const AddNoticeBtn = styled.button`
+  width: 28%;
+  height: 100%;
+  padding: 0 2%;
+  border: none;
+  border-radius: 8px;
+  color: white;
+  background-color: ${Colors.mainRed};
+  font-size: 25px;
+  font-weight: 500;
+
+  @media screen and (max-width: 960px) and (min-width: 500px) {
+    width: 100%;
+    font-size: 20px;
+  }
+`;
