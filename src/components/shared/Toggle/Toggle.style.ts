fde6794db1a1b0c22a35c991e0ddb9fa03848998
Wickhye <jihyesim170@gmail.com>
2023-03-22 08:33:17 +0900

    ADD :: Toggle

---
+++ src/components/shared/Toggle/Toggle.style.ts
@@ -0,0 +1,56 @@
+import styled, { css } from "styled-components";
+import { Colors } from "../../../constants/colors.constant";
+
+export const Toggle = styled.label`
+  display: inline-flex;
+  align-items: center;
+  color: ${Colors.mediumGray};
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+export const ToggleCircle = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 4px;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${Colors.backgroundColor};
+  transition: 0.5s;
+`;
+
+export const ToggleContainer = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 50px;
+  height: 30px;
+  border-radius: 27.5px;
+  background-color: ${Colors.mainRed};
+  margin-right: 4px;
+  padding: 0 7px;
+  transition: 0.5s;
+
+  &.disabled {
+    background-color: ${Colors.gray};
+
+    ${ToggleCircle} {
+      left: 4px;
+      transition: 0.5s;
+    }
+  }
+`;
+
+export const DisabledToggleStyle = css`
+  background-color: ${Colors.gray};
+
+  ${ToggleCircle} {
+    left: 4px;
+    transition: 0.5s;
+  }
+`;
+
+export const DisabledToggleContainer = styled(ToggleContainer)`
+  ${DisabledToggleStyle}
+`;
---
+++ src/components/shared/Toggle/Toggle.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import * as S from "./Toggle.style";
+import { LabelHTMLAttributes } from "react";
+
+// LabelHTMLAttributes<HTMLLabelElement> &
+
+type ToggleProps = {
+  value: any;
+  name?: string;
+  id?: string;
+  onClick?: () => void;
+  readOnly?: boolean;
+  label: string;
+};
+
+const Toggle = ({ label, onClick, value, name, id, readOnly }: ToggleProps) => {
+  return (
+    <>
+      <S.Toggle onClick={onClick}>
+        <input
+          type="hidden"
+          value={value}
+          name={name}
+          id={id}
+          readOnly={readOnly}
+        />
+        $
+        {value ? (
+          <S.ToggleContainer></S.ToggleContainer>
+        ) : (
+          <S.DisabledToggleContainer></S.DisabledToggleContainer>
+        )}
+        <S.ToggleCircle></S.ToggleCircle>
+        <span>{label}</span>
+      </S.Toggle>
+    </>
+  );
+};
+
+export default Toggle;
